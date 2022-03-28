b5f3568c0799ba00752a150689efe298b63c8337
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-28 18:16:01 +0300

    Добавьте файлы проекта.

---
+++ ParenthesisExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string staples;
+        char openParenthesis = '(';
+        char closeParenthesis = ')';
+        int openParenthesisCount = 0;
+        int closeParenthesisCount = 0;
+        int symbolsCount = 3;
+
+        staples = Console.ReadLine();
+
+        foreach (char symbol in staples)
+        {
+            if (openParenthesis == symbol)
+            {
+                openParenthesisCount++;
+            }
+            else if (closeParenthesis == symbol)
+            {
+                closeParenthesisCount++;
+            }
+            symbolsCount++;
+        }
+
+        if (openParenthesisCount == closeParenthesisCount)
+        {
+            Console.WriteLine($"Скобочное выражение верное и его максимальная глубина равняется {openParenthesisCount}");
+        }
+        else
+        {
+            Console.WriteLine("Выражение не корректно");
+        }
+    }
+}
